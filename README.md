6500c897259f7e7b6d73dc142a2265f1f3663b23
Diego sales <diegofelipesales23@gmail.com>
2024-04-30 15:23:20 -0400

    chore: add jest

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 ```
   npm i -D git-commit-msg-linter
   npm i -D typescript @types/node
+  npm i -D jest @types/jest ts-jest
 ```
---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type {Config} from 'jest';
+
+const config: Config = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: "coverage",
+  testEnvironment: "node",
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  }
+  //collectCoverage: true,
+  //coverageProvider: "v8",
+};
+
+export default config;
