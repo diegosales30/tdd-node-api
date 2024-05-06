7aa54ed9780ae8359a3d2ee9c6939d7719ed3854
Diego sales <diegofelipesales23@gmail.com>
2024-05-06 16:35:10 -0400

    test: ensure SignUpController return 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -212,6 +212,26 @@ describe('SignUpController', () => {
       const httpResponse = sut.handle(httpRequest);
       expect(httpResponse.statusCode).toBe(500);
       expect(httpResponse.body).toEqual(new ServerError());
-    });
+  });
 
+  //teste de validação de dados AddAccount caso sucesso
+  test('Should return 200 if valid data is provided', () => {
+    const {sut } = makeSut();
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_email@email.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password',
+      }
+    }
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password',
+    });
+  });
 })
